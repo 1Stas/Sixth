bffe26fb3486035e488d1dde6a6da4e65a087a66
Stas <afkfen7@gmail.com>
2019-07-28 13:00:32 +0300

    28 07 19

---
+++ app/js/common.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+	$('.popup_button').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#text',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#text';
+				}
+			}
+		}
+	});
+});
+
+$(document).ready(function() {
+	$('.navbar_form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#text',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#text';
+				}
+			}
+		}
+	});
+});
+// Карусель.
+
+$(document).ready(function(){
+	$(".info-carousel").owlCarousel({
+	//	nav:true,
+	//	dots:true,
+		loop:true,
+		center: true,
+	//	autoWidth:true,
+	//	items:1,
+	//	autoplay:true,
+	//	autoplayTimeout:2000,
+	//	smartSpeed:1900,
+		responsive: {0:{items:1}, 600:{items:2, center:false}, 900:{items:3}},
+	});
+	$(".about__carousel").owlCarousel({
+		nav:true,
+	//	dots:true,
+		loop:true,
+		center: true,
+	//	autoWidth:true,
+	//	items:1,
+	//	autoplay:true,
+	//	autoplayTimeout:2000,
+	//	smartSpeed:1900,
+		responsive: {0:{items:1}},
+	});
+});
+
+
+$(document).ready(function(){
+
+	$("#back-top").hide();
+	
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 1000) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+});
