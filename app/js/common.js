dbdcdeb54a297481f93970a85554decaa1941afa
Sec <1@mail.com>
2019-08-19 16:20:34 +0300

    19

--- app/js/common.js
+++ app/js/common.js
@@ -26,25 +26,75 @@ $(document).ready(function(){
 	});
 });
 
+// Range slider
 
-$(document).ready(function(){
-
-	$("#back-top").hide();
+	var image = document.querySelector('.change-photo');
+	var contrast = document.querySelector('.contrast-slide');
+	var brightness = document.querySelector('.brightness');
+	var blur = document.querySelector('.blur');
 	
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 1000) {
-				$('#back-top').fadeIn();
-			} else {
-				$('#back-top').fadeOut();
-			}
-		});
-
-		$('#back-top a').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
+	contrast.addEventListener('change', function(e){
+		image.style.filter = "contrast(" + e.target.value + "%" +")";
 	});
+	brightness.addEventListener('change', function(e){
+		image.style.filter = "brightness(" + e.target.value + "%" +")";
+	});
+	blur.addEventListener('change', function(e){
+		image.style.filter = "blur(" + e.target.value + "px" +")";
+	});
+
+// Своя иконка на картах. После блока код может не правильно работать
+
+ymaps.ready(function () {
+	var myMap = new ymaps.Map('map', {
+					center: [59.9360000, 30.3209000], // Y, X более 12000 (относительно центра) может выйти за пределы экрана на мобильных 
+					zoom: 17
+			}, {searchControlProvider: 'yandex#search'}),
+
+			// Создаём макет содержимого.
+			MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+					'<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+			),
+
+			myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+					hintContent: 'Собственный значок метки', //Отметка центра карты
+					balloonContent: 'Html Accademy'
+			}, {
+					// Опции.
+					// Необходимо указать данный тип макета.
+					iconLayout: 'default#image',
+					// Своё изображение иконки метки.
+					iconImageHref: '',
+					// Размеры метки.
+					iconImageSize: [30, 42],
+					// Смещение левого верхнего угла иконки относительно
+					// её "ножки" (точки привязки).
+					iconImageOffset: [-5, -38]
+			}),
+
+			myPlacemarkWithContent = new ymaps.Placemark([59.9360000, 30.3209000], {
+					hintContent: '',
+					balloonContent: 'Html Accademy',
+					iconContent: '' //Не забыть удалить
+			}, {
+					// Опции.
+					// Необходимо указать данный тип макета.
+					iconLayout: 'default#imageWithContent',
+					// Своё изображение иконки метки.
+					iconImageHref: '../img/svg/map-marker.svg',
+					// Размеры метки.
+					iconImageSize: [70, 120],
+					// Смещение левого верхнего угла иконки относительно
+					// её "ножки" (точки привязки).
+					iconImageOffset: [-30, -110],
+					// Смещение слоя с содержимым относительно слоя с картинкой.
+					iconContentOffset: [15, 15],
+					// Макет содержимого.
+					iconContentLayout: MyIconContentLayout
+			});
+
+	myMap.geoObjects
+			.add(myPlacemark)
+			.add(myPlacemarkWithContent);
 });
+
